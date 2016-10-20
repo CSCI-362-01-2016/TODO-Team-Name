3db03761fc28964e5538edfedb5c6aa5b98f4d57
lilerw <lilerw@g.cofc.edu>
2016-10-19 21:56:57 -0400

    cathcing up

--- TestAutomation/scripts/runAllTests.sh
+++ TestAutomation/scripts/runAllTests.sh
@@ -5,5 +5,14 @@ echo "Tests running"
 echo "run ../testCaseExecutables/TestSh.sh "
 
 ./testCaseExecutables/TestSh.sh
+echo ""
+echo ""
+echo ""
 
+echo "For Loop"
+echo ""
+for filename in testCases/*.txt; do
+	echo "$filename"
+
+done
 echo "done"
