806e46155c03cce22ca80382bbf86cba1903137a
Paige <peckpd@g.cofc.edu>
2016-11-09 20:20:17 -0500

    Fixed runTest1.sh to actually store value

--- TestAutomation/project/Jython/library/test1.py
+++ TestAutomation/project/Jython/library/test1.py
@@ -1,17 +1,16 @@
 from timer import Timer2
 import sys
-main:
+seconds = 0
+def echoTime():
+	global seconds
+	seconds = seconds + 1
+t = Timer2(1000, echoTime, [], True)
+t.start()
+if sys.argv[1]=='startTest':
+	print (str( t.isRunning()))
+t.stop()
 
-	seconds = 0
-	def echoTime():
-		global seconds
-		seconds = seconds + 1
-	t = Timer2(1000, echoTime, [], True)
-	t.start()
-	if sys.argv[1]=='startTest':
-		print (str( t.isRunning()))
-	t.stop()
-	return 'test'
+sys.exit('test')
 #print t.isRunning()
-#sys.exit(str(t.isRunning()))
+
 #print "\n"
--- TestAutomation/scripts/runTest1.sh
+++ TestAutomation/scripts/runTest1.sh
@@ -3,6 +3,8 @@
 clear
 sleep 1
 cd ../project/Jython/
-result=sh ./jython.sh -i library/test1.py "startTest"
+result=$(sh ./jython.sh -i library/test1.py "startTest" 2>&1)
+echo "\nResult:"
 echo "$result"
+echo "\nResult 2:"
 echo "$result"
