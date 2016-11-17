8103837958918e7eb9527a02af8e14f6830dba0a
lilerw <lilerw@g.cofc.edu>
2016-11-16 20:06:26 -0500

    Document RunAll Tests

--- TestAutomation/scripts/runAllTests.sh
+++ TestAutomation/scripts/runAllTests.sh
@@ -1,15 +1,19 @@
 #!/bin/bash
 #python ./testCaseExecutables/"$LINE" > temp/testCase"$i"results.txt
+
+#Clear the temp files
 echo "Clearing Temp Folder"
 rm -r temp/*
 rm -r reports/*
 
+#Preps final ouput file with /scripts/html-template.txt
 echo "Running Tests"
 echo " "
 while read LINE; do
 	echo "$LINE" >> reports/testReport.html
 done < "scripts/html-template.txt" 
 
+#reads each line of test case txt files and saves them into html
 for filename in testCases/*.txt; do
 	i="${filename//[^0-9]/}"
 	echo "Running Test $i"
@@ -18,10 +22,13 @@ for filename in testCases/*.txt; do
 		ARRAY+=("$LINE")
 	done < "$filename"
 	cd project/Jython/
+	#runs jython and applicable test case
 	OUTPUT=$(sh ./jython.sh -i  library/"${ARRAY[0]}" "${ARRAY[4]}" 2>&1)
 	cd ../../
+	#save output to temp txt files
 	echo "$OUTPUT" >> temp/Test"$i"temp.txt
 	
+	#find actual output from temp files
 	KEEPLINE="FALSE"
 	while read LINE; do
 		if [ "$KEEPLINE" == "True" ]; then
@@ -33,8 +40,7 @@ for filename in testCases/*.txt; do
 		    KEEPLINE="True"
 		fi
 	done < temp/Test"$i"temp.txt
-	#echo "OUTPUT________"
-	#echo "$OUTPUT"
+	#add info to final report for each test
 	echo "<tr>
 	<td>${ARRAY[0]}</td>
 	<td>${ARRAY[1]}</td>
@@ -43,7 +49,7 @@ for filename in testCases/*.txt; do
 	<td>${ARRAY[4]}</td>
 	<td>${ARRAY[5]}</td>
 	<td>$OUTPUT</td>" >> temp/testcase"$i"result.html
-
+	#Compare expected and actual output and store result in final report
         if [ "${ARRAY[5]}" == "$OUTPUT"  ]; then
 		echo "<td>Pass</td></tr>" >> temp/testcase"$i"result.html
 	else
@@ -60,6 +66,6 @@ done
 echo "</table>" >> reports/testReport.html
 echo "</body>" >> reports/testReport.html
 echo "</html>" >> reports/testReport.html
-
+#open html file
 xdg-open reports/testReport.html
 echo "done"
